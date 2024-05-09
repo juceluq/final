879de7f8a9889137a7126edf57de05a2d43af748
Juan Antonio Uceda Luque <juceluq@g.educaand.es>
2024-05-09 16:25:29 +0200

    DatabaseSeeder tiene ahora Establishments y más usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Establishment;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,14 +14,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        // Crear usuarios
+        $adminUser = User::factory()->create([
             'name' => 'Test User',
             'email' => 'juanminiyo@outlook.com',
-            'username' => 'juan',
-            'password' => 'a',
+            'username' => 'admin',
+            'password' => 'admin',
             'role' => 'admin'
         ]);
+
+        $businessUser = User::factory()->create([
+            'name' => 'Business User',
+            'email' => 'business@example.com',
+            'username' => 'business',
+            'password' => 'business',
+            'role' => 'business'
+        ]);
+
+        $clientUser = User::factory()->create([
+            'name' => 'Client User',
+            'email' => 'client@example.com',
+            'username' => 'client',
+            'password' => 'client',
+            'role' => 'client'
+        ]);
+
+        // Crear establecimientos
+        foreach (range(1, 12) as $index) {
+            Establishment::factory()->create([
+                'user_id' => $businessUser->id,
+                'name' => "Business $index",
+                'description' => "Description for Business $index",
+                'location' => "Location for Business $index",
+                'category' => "Category for Business $index",
+                'image' => 'images/default.jpg'
+            ]);
+        }
     }
 }
