881592d6cb96642d583c7898e56189b45cdfd06d
Juan Antonio Uceda Luque <juceluq@g.educaand.es>
2024-05-15 14:43:41 +0200

    cambiada la zona horaria

--- resources/views/establishment/establishment.blade.php
+++ resources/views/establishment/establishment.blade.php
@@ -452,7 +452,7 @@
         <div id="editReviewModal" class="hidden fixed z-10 inset-0 overflow-y-auto">
             <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <div class="fixed inset-0 transition-opacity" aria-hidden="true">
-                    <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                    <div class="absolute inset-0 bg-gray-500 opacity-75" onclick="closeEditModal()"></div>
                 </div>
 
                 <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
