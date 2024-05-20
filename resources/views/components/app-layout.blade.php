99e7a3f7b1359c0c817a32dc164fbdff88857c22
Juan Antonio Uceda Luque <juceluq@g.educaand.es>
2024-05-20 10:20:13 +0200

    Minor changes

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -34,7 +34,7 @@
                         </div>
                         <input type="search" id="default-search" name="query"
                             class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
-                            placeholder="Search Establishments" required />
+                            placeholder="Search Establishments..." required />
                         <button type="submit"
                             class="text-white absolute end-2.5 bottom-2.5 bg-indigo-600 dark:bg-indigo-700 hover:bg-indigo-800 focus:ring-4 font-medium rounded-lg text-sm px-4 py-2 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:text-gray-300 border border-gray-300 dark:border-gray-600">Search</button>
                     </div>
